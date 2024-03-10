c036c6acca7936c086143437494bdf124612c778
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-03-10 17:01:09 +0000

    MVP fixed

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -4,24 +4,59 @@ public class BankAccount {
 
     private String firstName;
     private String lastName;
-    private LocalDate dateOfBirth;
+    private String dateOfBirth;
     private int accountNumber;
     private double balance;
+    private int overdraft;
 
-    private double amount;
+    private String accountType;
 
+//    private double amount;
 
+    // ** create constructor
+    public BankAccount(String firstName, String lastName, String dateOfBirth, int accountNumber,double balance, int overdraft, String accountType) {
+        //   property     parameter
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.accountType = accountType;
 
-    public static String getFirstName(String firstName) {
-        return firstName;
     }
 
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+    public void withdraw(int amount) {
+
+        this.balance -= amount;
+
+    }
+
+    public double calculateInterest(double percentage) {
+        double interestPay = this.balance * percentage;
+        return interestPay;
+
+    }
+    public void payInterest(double percentage) {
+        double interestToPay = this.balance * percentage;
+        this.balance += interestToPay;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public static String getLastName(String lastName) {
+    public String getLastName() {
         return lastName;
     }
 
@@ -29,15 +64,15 @@ public class BankAccount {
         this.lastName = lastName;
     }
 
-    public static int getDateOfBirth(int year, int month, int day) {
-        return year + month + day;
+    public String getDateOfBirth() {
+        return dateOfBirth;
     }
 
-    public void setDatOfBirth(LocalDate dateOfBirth) {
+    public void setDateOfBirth(String  dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
-    public static int getAccountNumber(int accountNumber) {
+    public int getAccountNumber() {
         return accountNumber;
     }
 
@@ -45,26 +80,78 @@ public class BankAccount {
         this.accountNumber = accountNumber;
     }
 
-    public static double getBalance(double balance) {
-        return balance;
-    }
-
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
-    public static double deposit(double balance, double deposit) {
-        return balance + deposit;
-    }
-    public static double withdrawal(double balance, double withdrawal) {
-        return balance - withdrawal;
+    public int getOverdraft() {
+        return overdraft;
     }
 
-    public static double interest(double balance) {
-        double interest = 0.04 * balance;
-        return balance + interest;
-
+    public void setOverdraft(int overdraft) {
+        this.overdraft = overdraft;
     }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+    //    public static String getFirstName(String firstName) {
+//        return firstName;
+//    }
+//
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public static String getLastName(String lastName) {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public static int getDateOfBirth(int year, int month, int day) {
+//        return year + month + day;
+//    }
+//
+//    public void setDatOfBirth(LocalDate dateOfBirth) {
+//        this.dateOfBirth = dateOfBirth;
+//    }
+//
+//    public static int getAccountNumber(int accountNumber) {
+//        return accountNumber;
+//    }
+//
+//    public void setAccountNumber(int accountNumber) {
+//        this.accountNumber = accountNumber;
+//    }
+//
+//    public static double getBalance(double balance) {
+//        return balance;
+//    }
+//
+//    public void setBalance(double balance) {
+//        this.balance = balance;
+//    }
+//
+//    public static double deposit(double balance, double deposit) {
+//        return balance + deposit;
+//    }
+//    public static double withdrawal(double balance, double withdrawal) {
+//        return balance - withdrawal;
+//    }
+//
+//    public static double payInterest(double balance) {
+//        double interest = 0.04 * balance;
+//        return balance + interest;
+//
+//    }
 
 
 
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,3 +1,5 @@
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -5,52 +7,142 @@ import java.time.LocalDate;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BankAccountTest {
+
+    BankAccount bankAccount;
+
+    @BeforeEach
+    public void setup() {
+        //                                                             need LocalDate ****
+        bankAccount = new BankAccount("Tommy", "Bui", "01/01/2000", 00001234, 0.00,-50, "current");
+    }
+
     @Test
-    public void canReturnFirstName() {
-        String result = BankAccount.getFirstName("Tommy");
+    public void canDeposit() {
+        bankAccount.deposit(100);
+        assertThat(bankAccount.getBalance()).isEqualTo(100);
+        // create deposit and get balance - TDD method
+    }
+    // balance == 0 as before each wipes out the deposit
+    @Test
+    public void canWithdraw() {
+        bankAccount.withdraw(125);
+        assertThat(bankAccount.getBalance()).isEqualTo(-125);
 
     }
 
     @Test
-    public void canReturnLastName() {
-        String result = BankAccount.getLastName("Bui");
-
+    public void canPayInterest() {
+        bankAccount.deposit(100);
+        bankAccount.payInterest(0.02);
+        assertThat(bankAccount.getBalance()).isEqualTo(102);
     }
 
     @Test
-    public void canReturnDateOfBirth() {
-        LocalDate result = LocalDate.ofEpochDay(BankAccount.getDateOfBirth(2000, 1, 28));
+    public void hasFirstName() {
+        assertThat(this.bankAccount.getFirstName()).isEqualTo("Tommy");
     }
 
     @Test
-    public void canReturnAccountNumber() {
-        int result = BankAccount.getAccountNumber(00001234);
-
+    public void canSetFirstName() {
+        this.bankAccount.setFirstName("Tommy");
+        assertThat(this.bankAccount.getFirstName()).isEqualTo("Tommy");
     }
 
     @Test
-    public void canReturnBalance() {
-        double result = BankAccount.getBalance(0.00);
+    public void hasLastName() {
+        assertThat(this.bankAccount.getLastName()).isEqualTo("Bui");
     }
 
     @Test
-    public void deposit() {
-        BankAccount bankAccount = new BankAccount();
-        double expected = 45.00;
-        double actual = BankAccount.deposit(0.00, 45.00);
-
-        assertThat(actual).isEqualTo(expected);
+    public void canSetLastName() {
+        this.bankAccount.setLastName("lalala");
+        assertThat(this.bankAccount.getLastName()).isEqualTo("lalala");
     }
 
     @Test
-    public void withdrawal() {
-        BankAccount bankAccount = new BankAccount();
-        double expected = 0.00;
-        double actual = BankAccount.withdrawal(45.00, 45.00);
-
-        assertThat(actual).isEqualTo(expected);
-
-
+    public void hasDateOfBirth() {
+        assertThat(this.bankAccount.getDateOfBirth()).isEqualTo("01/01/2000");
     }
+
+    @Test
+    public void canSetDateOfBirth() {
+        this.bankAccount.setDateOfBirth("02/01/2000");
+        assertThat(this.bankAccount.getDateOfBirth()).isEqualTo("02/01/2000");
+    }
+
+    @Test
+    public void hasAccountNumber() {
+        assertThat(this.bankAccount.getAccountNumber()).isEqualTo(00001234);
+    }
+
+    @Test
+    public void canSetAccountNumber() {
+        this.bankAccount.setAccountNumber(0000000);
+        assertThat(this.bankAccount.getAccountNumber()).isEqualTo(0000000);
+    }
+
+    @Test
+    public void canGetBalance() {
+        assertThat(this.bankAccount.getBalance()).isEqualTo(0);
+    }
+
+    @Test
+    public void canSetBalance() {
+        this.bankAccount.setBalance(100.0);
+        assertThat(this.bankAccount.getBalance()).isEqualTo(100.0);
+    }
+
+
+
+
+
+    // ------------------------------------ Self-work
+//    @Test
+//    public void canReturnFirstName() {
+//        String result = BankAccount.getFirstName("Tommy");
+//
+//    }
+//
+//    @Test
+//    public void canReturnLastName() {
+//        String result = BankAccount.getLastName("Bui");
+//
+//    }
+//
+//    @Test
+//    public void canReturnDateOfBirth() {
+//        LocalDate result = LocalDate.ofEpochDay(BankAccount.getDateOfBirth(2000, 1, 28));
+//    }
+//
+//    @Test
+//    public void canReturnAccountNumber() {
+//        int result = BankAccount.getAccountNumber(00001234);
+//
+//    }
+//
+//    @Test
+//    public void canReturnBalance() {
+//        double result = BankAccount.getBalance(0.00);
+//    }
+//
+//    @Test
+//    public void deposit() {
+//        BankAccount bankAccount = new BankAccount();
+//        double expected = 45.00;
+//        double actual = BankAccount.deposit(0.00, 45.00);
+//
+//        assertThat(actual).isEqualTo(expected);
+//    }
+//
+//    @Test
+//    public void withdrawal() {
+//        BankAccount bankAccount = new BankAccount();
+//        double expected = 0.00;
+//        double actual = BankAccount.withdrawal(45.00, 45.00);
+//
+//        assertThat(actual).isEqualTo(expected);
+//
+//
+//    }
 
 }
